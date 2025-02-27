b81e07145ea52e9725a1e250dbe2b58f759a35cc
AzzaBenRomdhan <benromdhan.azza99@gmail.com>
2025-02-27 15:05:56 +0100

    updating devyml and compose files

--- Backend/src/main/java/com/azza/jwtauthentication/email/EmailService.java
+++ Backend/src/main/java/com/azza/jwtauthentication/email/EmailService.java
@@ -53,7 +53,7 @@ public class EmailService {
         Context context = new Context();
         context.setVariables(properties);
 
-        helper.setFrom("celina.celine@gmail.com");
+        helper.setFrom("benromdhan.azza99@gmail.com");
         helper.setTo(to);
         helper.setSubject(subject);
 
